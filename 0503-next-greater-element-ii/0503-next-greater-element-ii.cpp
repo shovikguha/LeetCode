f1e45f65a13884cd78a2c52e6c8c47208f458272
Shovik Guha <shovik.guha@gmail.com>
2022-11-09 10:27:30 -0800

    Time: 81 ms (47.22%), Space: 24.2 MB (35.33%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<pair<int,int>>st;
+        
+        int n = nums.size();
+        vector<int> ret(n,-1);  
+        
+        for(int i =0; i < n; i++){
+            while(!st.empty() && st.top().first < nums[i]){
+                auto p = st.top();
+                ret[p.second] = nums[i];
+                st.pop();
+            }
+            st.push(make_pair(nums[i],i));
+        }
+        
+        for(int i =0; i < n; i++){
+            while(!st.empty() && st.top().first < nums[i]){
+                auto p = st.top();
+                if(ret[p.second] == -1){
+                    ret[p.second] = nums[i];
+                }
+                st.pop();
+            }
+            st.push(make_pair(nums[i],i));
+        }
+        
+        return ret;
+    }
+};
