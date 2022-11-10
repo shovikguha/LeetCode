837db9569bb0d7b2bf388ec843e04f6da5036eae
Shovik Guha <shovik.guha@gmail.com>
2022-11-10 10:36:07 -0800

    Time: 295 ms (8.63%), Space: 59 MB (36.67%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i = 0;
+        int j = height.size() -1;
+        int mx = 0;
+        
+        while(i <= j){
+            mx = max(mx, (j-i)*min(height[i], height[j]));
+            if(height[i] < height[j]){
+                i += 1;
+            }
+            else{
+                j -= 1;
+            }
+        }
+        
+        return mx;
+    }
+};
