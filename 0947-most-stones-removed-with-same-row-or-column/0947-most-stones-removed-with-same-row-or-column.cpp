3ae2bd3a2bcd0f3f6046ba1c47332c1a48d331d9
Shovik Guha <shovik.guha@gmail.com>
2022-11-14 09:58:34 -0800

    Time: 294 ms (49.32%), Space: 16.8 MB (51.22%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     int removeStones(vector<vector<int>>& stones) {
         int n = stones.size();
-        
         vector<vector<int>> adjlst(n);
         
+        
         for(int i = 0; i < n; i++){
-            for(int j = i+1; j< n; j++){
-                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
+            for(int j = i + 1; j < n; j++){
+                if((stones[i][0] == stones[j][0]) || (stones[i][1] == stones[j][1])){
                     adjlst[i].push_back(j);
                     adjlst[j].push_back(i);
                 }
             }
         }
         
-        vector<int> visited(n);
+        vector<int> seen(n);
         int comps = 0;
         for(int i = 0; i < n; i++){
-            if(visited[i] == 0){
-                dfs(i, visited, adjlst);
+            if(seen[i] == 0){
+                dfs(i, seen, adjlst);
                 comps++;
             }
         }
@@ -26,12 +26,14 @@ public:
         return n - comps;
     }
     
-    void dfs(int node, vector<int>& visited, vector<vector<int>>& adjlst){
-        if(visited[node] == 0){
-            visited[node] = 1;
-            for(int ne : adjlst[node]){
-                dfs(ne, visited, adjlst);
+    void dfs(int i, vector<int>& seen, vector<vector<int>>& adjlst){
+        seen[i] = 1;
+        
+        for(int ne : adjlst[i]){
+            if(seen[ne] == 0){
+                dfs(ne, seen, adjlst);
             }
         }
     }
+    
 };
