f0e4261916112cce21cba337ad6ecd5f28983943
Shovik Guha <shovik.guha@gmail.com>
2022-12-21 10:35:28 -0800

    Time: 529 ms (21.71%), Space: 73.1 MB (15.09%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        vector<vector<int>> adjlst(n);
+        vector<vector<int>> edges(n);
         
-        for(auto& v : dislikes){
-            adjlst[v[0]-1].push_back(v[1]-1);
-            adjlst[v[1]-1].push_back(v[0]-1);
+        for(vector<int> v : dislikes){
+            edges[v[0]-1].push_back(v[1]-1);
+            edges[v[1]-1].push_back(v[0]-1);            
         }
-        
-        vector<int> colors(n);
-        for(int i =0; i < n; i++){
-            colors[i] = -1;
-        }
-        
+                  
         queue<pair<int,int>> q;
-        
+        unordered_map<int,int> seen;
+                  
         for(int i = 0; i < n; i++){
-            if(colors[i] == -1){
-               q.push(make_pair(i,1));
+            if(seen.find(i) == seen.end()){
+                seen[i] = 0;
+                if(!bfs(i, seen, edges)){
+                    return false;
+                }
+            }
+        }
+                  
+        return true;
+    }
+                  
+    bool bfs(int i,  unordered_map<int,int>& seen, vector<vector<int>>& edges){
+        queue<pair<int,int>> q;
+        q.push(make_pair(i, 0));
         
-                while(!q.empty()){
-                    auto p = q.front();
-                    q.pop();
-                    colors[p.first] = p.second;
-                    for(int ne : adjlst[p.first]){
-                        if(colors[ne] != (1 - p.second)){
-                            if(colors[ne] == p.second){
-                                return false;
-                            }
-                            else{
-                                q.push(make_pair(ne, 1 - p.second));
-                            }
-                        }
+        while(!q.empty()){
+            pair<int,int> p = q.front();
+            q.pop();
+            
+            for(int ne : edges[p.first]){
+                if(seen.find(ne) == seen.end()){
+                    seen[ne] = (1 - p.second);
+                    q.push(make_pair(ne, 1 - p.second));
+                }
+                else{
+                    if(seen[ne] != 1 - p.second){
+                        return false;
                     }
-                } 
+                }
             }
         }
         
