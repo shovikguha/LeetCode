28e3cfba7597fd4325c02056bf249ca858bb1c6c
Shovik Guha <shovik.guha@gmail.com>
2023-01-05 10:27:20 -0800

    Time: 53 ms (27.27%), Space: 14.1 MB (45.05%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adjlst(numCourses);
+        vector<vector<int>> edges(numCourses);
         vector<int> indegree(numCourses);
-        vector<int> ret;
         
-        for(auto& v  : prerequisites){
-            adjlst[v[1]].push_back(v[0]);
+        for(vector<int> v : prerequisites){
+            edges[v[1]].push_back(v[0]);
             indegree[v[0]]++;
         }
         
         queue<int> q;
+        vector<int> ret;
         for(int i = 0; i < numCourses; i++){
             if(indegree[i] == 0){
                 q.push(i);
+                ret.push_back(i);
             }
         }
-        
-        vector<int> seen(numCourses);
         
         while(!q.empty()){
             int curr = q.front();
             q.pop();
-            seen[curr] = 1;
-            ret.push_back(curr);
             
-            for(int ne : adjlst[curr]){
+            for(int ne : edges[curr]){
                 indegree[ne]--;
                 if(indegree[ne] == 0){
                     q.push(ne);
+                    ret.push_back(ne);
                 }
             }
         }
         
-        for(int i = 0; i < numCourses; i++){
-            if(seen[i] == 0){
+        for(int i : indegree){
+            if(i != 0){
                 return {};
             }
         }
